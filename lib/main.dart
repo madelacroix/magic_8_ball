96ad651ee0deb8e22feb1f74e96f2bfa57be1b94
Madel dela Cruz <madeldelacruz@Madels-MacBook-Pro.local>
2021-12-26 11:59:09 +1300

    magic 8 ball challenge app

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask me Anything'),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: BallPage(),
+        ),
+      ),
+    );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  ballNumber = Random().nextInt(5) + 1;
+                  print('I got clicked');
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
